3289f129bb94b6aae5ef77a77429d6bc14c981ba
ramesh <bharatha.ramesh@globallogic.com>
2022-09-20 12:20:21 +0530

    new commit

--- product-webapp/telecareapp/src/environment.js
+++ product-webapp/telecareapp/src/environment.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "http://3.90.12.52:8080";
-export const BASE_URL = "http://3.90.12.52:8080";
+//export const BASE_URL = "http://3.222.220.179:8080";
+export const BASE_URL = "http://3.222.220.179:8080";
