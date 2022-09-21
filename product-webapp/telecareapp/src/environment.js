8695caff3f8f682fabb3abe6cd0d78a49c9782dd
Ravi <ravikirang000@gmail.com>
2022-09-21 15:36:03 +0530

    ravi

--- product-webapp/telecareapp/src/environment.js
+++ product-webapp/telecareapp/src/environment.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "http://3.222.220.179:8080";
-export const BASE_URL = "http://3.222.220.179:8080";
+//export const BASE_URL = "http://54.221.14.248:8080";
+export const BASE_URL = "http://54.221.14.248:8080";
